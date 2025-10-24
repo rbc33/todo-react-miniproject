eb60411b3cada8ef70c4bf43614d8a26624abda4
rbc33 <ricardobenthem@gmail.com>
2025-10-24 17:11:08 +0200

    style TodoDetail

--- src/components/TodoDetail.tsx
+++ src/components/TodoDetail.tsx
@@ -6,7 +6,8 @@ const TodoDetail = () => {
 	const { todos, toggleTodo, updateTodo } = useTodoStore()
 	const todo = todos.find((t) => t.id === parseInt(id!))
 	return (
-		<>
+		<div className="text-3xl space-x-2">
+			<label>Task: </label>
 			<input
 				type="text"
 				placeholder={todo?.task}
@@ -15,13 +16,13 @@ const TodoDetail = () => {
 			/>
 			<label htmlFor={todo!.id.toString()}>Completed:</label>
 			<input
-				className="size-3 ml-2"
+				className="size-6 ml-2"
 				type="checkbox"
 				checked={todo!.completed}
 				onClick={() => toggleTodo(todo!.id)}
 				id={todo!.id.toString()}
 			/>
-		</>
+		</div>
 	)
 }
 
