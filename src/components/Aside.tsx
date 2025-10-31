2edd1769fbf499dc03c1621381bf484f6b08ae25
rbc33 <ricardobenthem@gmail.com>
2025-10-31 18:34:09 +0100

    change showing to NavLink isActive

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,38 +1,36 @@
-import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+// import { useLocation } from 'react-router-dom'
 
 const Aside = () => {
-	const location = useLocation()
+	// const location = useLocation()
 	return (
 		<aside className="w-35 -mt-0.5 flex flex-col gap-4 border-2 border-slate-400 h-screen p-5 font-bold text-xl">
-			<Link
-				className={
-					location.pathname === '/' ? 'text-slate-400' : 'hover:text-slate-400!'
+			<NavLink
+				className={({ isActive }) =>
+					isActive ? 'text-slate-400' : 'hover:text-slate-400!'
 				}
 				to="/"
 			>
 				Home
-			</Link>
-			<Link
-				className={
-					location.pathname === '/addtodo'
+			</NavLink>
+			<NavLink
+				className={({ isActive }) =>
+					isActive
 						? 'whitespace-nowrap text-slate-400'
 						: 'whitespace-nowrap hover:text-slate-400!'
 				}
 				to="/addtodo"
 			>
 				New Todo
-			</Link>
-			<Link
-				className={
-					location.pathname === '/about'
-						? 'text-slate-400'
-						: 'hover:text-slate-400!'
+			</NavLink>
+			<NavLink
+				className={({ isActive }) =>
+					isActive ? 'text-slate-400' : 'hover:text-slate-400!'
 				}
 				to="/about"
 			>
 				About
-			</Link>
+			</NavLink>
 		</aside>
 	)
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className="fixed bottom-0 -ml-35 h-10 w-screen text-center ">
 			<a
-				className="text-xl hover:text-blue-400 hover:underline"
+				className="text-xl hover:text-slate-400 hover:underline"
 				href="https://github.com/rbc33/todo-react-miniproject.git"
 			>
 				Github repo
