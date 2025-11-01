f5f2f06fc8cc0b794520da7e10f3945fb7820aa6
rbc33 <ricardobenthem@gmail.com>
2025-11-01 11:58:54 +0100

    footer at the en of the page, but aside dosent go to the bottom at addTodo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="fixed bottom-0 -ml-35 h-10 w-screen text-center ">
+		<footer className="w-full h-12 bg-gray-800 text-center flex items-center justify-center mt-auto">
 			<a
 				className="text-xl hover:text-slate-400 hover:underline"
 				href="https://github.com/rbc33/todo-react-miniproject.git"
