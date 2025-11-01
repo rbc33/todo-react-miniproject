ae5b603efd18ea440face0a33bf235495bd90507
rbc33 <ricardobenthem@gmail.com>
2025-11-01 12:10:16 +0100

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="w-full h-12 bg-gray-800 text-center flex items-center justify-center mt-auto">
+		<footer className="w-screen h-12 -ml-35 text-center flex items-center justify-center mt-auto">
 			<a
 				className="text-xl hover:text-slate-400 hover:underline"
 				href="https://github.com/rbc33/todo-react-miniproject.git"
