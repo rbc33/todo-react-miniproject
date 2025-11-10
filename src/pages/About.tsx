84acf3813f8cb49ca03fefc5f2cf944924acad47
rbc33 <ricardobenthem@gmail.com>
2025-11-10 23:11:31 +0100

    updated about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ const About = () => {
 		<div className="text-2xl p-5 w-[60vw] leading-relaxed">
 			<h2 className="text-3xl font-bold mb-4">Kanban Board Application</h2>
 			<p className="mb-4">
-				A modern Kanban board built with <strong>React</strong>,{' '}
+				A Kanban board built with <strong>React</strong>,{' '}
 				<strong>Electron</strong>, <strong>TypeScript</strong>, and{' '}
 				<strong>Tailwind CSS</strong>.
 			</p>
@@ -13,9 +13,9 @@ const About = () => {
 				connected to a JSON Server deployed on a VPS.
 			</p>
 			<p>
-				Enhanced with custom <strong>Google Fonts</strong> integration and
-				elegant <strong>Flowbite</strong> table components for a polished user
-				experience.
+				Enhanced with custom <strong>Google Fonts</strong> integration, elegant{' '}
+				<strong>Flowbite</strong> table components, and loading skeletons to
+				improve user experience during data fetching.
 			</p>
 		</div>
 	)
