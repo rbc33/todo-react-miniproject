9473017ea87c5aaefe4d8b71ad172ab1d7fade48
rbc33 <ricardobenthem@gmail.com>
2025-11-10 15:58:07 +0100

    Updated About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,10 @@
 const About = () => {
 	return (
 		<div className=" text-2xl p-5 w-[60vw]">
-			This is Kanban board built with React, TypeScript, and Tailwind CSS.
-			Features drag-and-drop functionality, Zustand state management with
-			persistance using Local storage, and custom Google Fonts integration.
+			This is Kanban board built with React, electron, TypeScript, and Tailwind
+			CSS. Features drag-and-drop functionality, Zustand state management, CRUD
+			operation with a JSON Server deployed in a vps and custom Google Fonts
+			integration.
 		</div>
 	)
 }
